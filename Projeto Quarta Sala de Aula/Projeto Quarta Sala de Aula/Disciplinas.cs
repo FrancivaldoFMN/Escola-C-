e2bb901fb657521ec31943b6a5dc1b4920cd0b7c
Francivaldo Monteiro do Nascimento <79109948+FrancivaldoFMN@users.noreply.github.com>
2022-04-29 22:34:08 -0300

    Add files via upload

---
+++ Projeto Quarta Sala de Aula/Projeto Quarta Sala de Aula/Disciplinas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Quarta_Sala_de_Aula
+{
+    internal class Disciplinas
+    {
+
+        public string Sala { get; set; }
+        public string NomeProfessor { get; set; }
+
+    }
+}
---
+++ Projeto Quarta Sala de Aula/Projeto Quarta Sala de Aula/Materias.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Quarta_Sala_de_Aula
+{
+    internal class Materias
+    {
+        public string IDMateria { get; set; }
+        public string TipoMateria { get; set; }
+
+    }
+}
